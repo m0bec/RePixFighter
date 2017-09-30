7a9cfaa398513471e0cf61580191bdb4cb6a2d31
mobec <mobec622@outlook.jp>
2017-09-30 15:22:35 +0900

    ディレクトリの移動&Load画面の実装

--- RePixelFighter/Assets/src/Title/TextStart.cs
+++ RePixelFighter/Assets/src/Title/TextStart.cs
@@ -5,10 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class TextStart : BaseTitleMenueSelect {
 
+	GameObject load_graph;
 	// Use this for initialization
 	protected override void Start () {
 		base.Start();
 		base.StateNum = (int)TitleSelectMenueSys.TitleMenueStateName.start;
+		load_graph = GameObject.Find("LoadDisp");
+		load_graph.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -21,12 +24,17 @@ public class TextStart : BaseTitleMenueSelect {
 
 	void Action(){
 		if(Input.GetKeyDown(KeyCode.Return)){
-			StartCoroutine("AsynLoad");
+			load_graph.SetActive(true);
+			StartCoroutine(AsynLoad());
 		}
 	}
 
-	private IEnumerator AsynLoad()
-	{
-    	yield return SceneManager.LoadSceneAsync("SelectDifficult");
+	AsyncOperation async;
+	private IEnumerator AsynLoad(){
+    	async = SceneManager.LoadSceneAsync("SelectDifficult");
+
+		while(!async.isDone){
+			yield return null;
+		}
 	}
 }
