fb704cf7e136b38bec2a3c650ffc82a824e1a03b
mobec <mobec622@outlook.jp>
2017-11-24 00:13:30 +0900

    tankがスクロールに合わせて移動する

--- RePixelFighter/Assets/src/Enemy/TankUnder.cs
+++ RePixelFighter/Assets/src/Enemy/TankUnder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TankUnder : BaseEnemy {
-
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +11,10 @@ public class TankUnder : BaseEnemy {
 	// Update is called once per frame
 	protected override void Update () {
 		base.Update();
+		if(base.data_keeper.StageNum == (int)Stage.One){
+			Vector3 pos = this.transform.position;
+			pos.y -= ScrollSpeedStruct.One;
+			this.transform.position = pos;
+		}
 	}
 }
