c5137153852b9be373393f4bd5663e4de0d9800b
mobec <mobec622@outlook.jp>
2017-12-12 18:29:59 +0900

    middle bossを実装準備

--- RePixelFighter/Assets/src/Enemy/BaseCreateEnemy.cs
+++ RePixelFighter/Assets/src/Enemy/BaseCreateEnemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum EnemyType{
-	et_fighter_bg, tank_obj, ball_obj
+	et_fighter_bg, tank_obj, ball_obj, middle_boss
 }
 
 public class BaseCreateEnemy : MonoBehaviour {
@@ -26,7 +26,20 @@ public class BaseCreateEnemy : MonoBehaviour {
 				CreateBallEnemy(create_pos_, hp_, score_, move_type_, shot_type_, bullet_type_, bullet_color_, bullet_speed_, move_speed_,
 				  enemy_move_controller_, enemy_shot_controller_);
 				break;
+
+			case (int)EnemyType.middle_boss:
+				CreateMiddleBossEnemy(create_pos_, hp_, score_, move_type_, shot_type_, bullet_type_, bullet_color_, bullet_speed_, move_speed_,
+				  enemy_move_controller_, enemy_shot_controller_);
+				break;
 		}
+	}
+
+	public GameObject middle_boss_one_obj;
+	protected void CreateMiddleBossEnemy(Vector3 create_pos_, float hp_, int score_, int move_type_, int shot_type_, int bullet_type_, int bullet_color_,
+	 float bullet_speed_, float move_speed_, GameObject enemy_move_controller_, GameObject enemy_shot_controller_){
+		str_obj = Instantiate(middle_boss_one_obj, create_pos_, Quaternion.Euler(30.0f, 0.0f, 0.0f));
+		str_obj.GetComponent<MiddleEnemyOne>().SetStatus(hp_, score_, move_type_, shot_type_, bullet_type_, bullet_speed_, move_speed_,
+	 	enemy_move_controller_, enemy_shot_controller_);
 	}
 
 	public GameObject ball_obj;
---
+++ RePixelFighter/Assets/src/Enemy/MiddleEnemyOne.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleEnemyOne : BaseEnemy {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update();
+	}
+}
